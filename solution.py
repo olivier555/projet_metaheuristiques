7c4898f01c5c1710af70b46b8c5d6d216b2725ee
= <=bastien.rolland@eleves.enpc.fr>
2018-10-20 17:24:05 +0200

    add solution.py

---
+++ solution.py
@@ -0,0 +1,45 @@
+class Solution():
+	def __init__(self, n, sensors = 0):
+		if sensors == 0:
+			sensors = np.zeros(n,dtype = 'bool')
+		else:
+			sensors = np.array(sensors,'bool')
+			assert(sensors.size == n, "size of sensors must be equal to n")
+		self.sensors = sensors
+		self.value = self.compute_value()
+
+	def compute_value(self):
+		return sum(self.sensors == 1)
+
+	def detected(self, data):
+		M = data.get_matrix_sens()
+		return all(M*self.sensors >= 1)
+
+
+	def related(self, data):
+		reached = {0}
+		next_vertex = set{data.neighbours(0)}
+		index_sensors = np.where(sensors)
+		while len(next_vertex) > 0 and len(reached) < self.value:
+			v = {}
+			for i in next_vertex :
+				new = intersection(set(data.neighbours_sens(i)),index_sensors)
+				new = new - reached
+				reached = union(reached,new)
+				v = v.union(new)
+			next_vertex = v
+		return len(reached)== self.value
+
+
+	def eligible(self, data):
+		return self.detected(data) and self.related(data)
+
+	def add_sensor(self,i):
+		if not self.sensors[i]:
+			self.sensors[i] = True
+			self.value += 1
+
+	def remove_sensor(self,i):
+		if self.sensors[i]:
+			self.sensors[i] = False
+			self.value -= 1
