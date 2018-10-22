a75fc73475e5d568e3c4db33ba757065220736a1
= <=bastien.rolland@eleves.enpc.fr>
2018-10-22 11:23:14 +0200

    add kruskal to graph and update solutions

--- graph.py
+++ graph.py
@@ -64,13 +64,13 @@ class Graph():
 		else:
 			assert isinstance(edges_value,np.ndarray), "type of edges_value not known"
 			assert edges_value.shape == (self.n,self.n), "edges_value matrix shape doesn't fit n"
-			assert isinstance(adjacency,np.ndarray) and adjacency.shape == (n,n), "adjacency is not a square matrix of size n"
+			assert isinstance(adjacency,np.ndarray) and adjacency.shape == (self.n,self.n), "adjacency is not a square matrix of size n"
 			if self.edges_value_matrix :
 				self.edges_value_m = edges_value
 			if self.edges_value_dic :
 				self.edges_value_d = {}
-				for i in range(n):
-					for j in range(i+1,n):
+				for i in range(self.n):
+					for j in range(i+1,self.n):
 						if adjacency[i][j]:
 							self.edges_value_d[(i,j)] = edges_value[i][j]
 							self.edges_value_d[(j,i)] = edges_value[j][i]
@@ -124,6 +124,53 @@ class Graph():
 
 	def get_size(self):
 		return self.n
+
+	def kruskal(self):
+		assert not(self.oriented)
+		list_value_edges = []
+		list_index_edges = []
+		for i in range(self.n):
+			for j in self.neighbours[i]:
+				if i < j:
+					list_value_edges.append(self.edges_value_d[(i,j)])
+					list_index_edges.append((i,j))  
+		# half_edges_value = np.triu(self.edges_value_m)
+		# half_edges_value = half_edges_value.reshape(self.n**2)
+		sorted_index = np.argsort(list_value_edges)
+		print(sorted_index)
+		print(list_value_edges[sorted_index[0]])
+		compt = 0
+		ind = 0
+		new_neighbours = {}
+		for i in range(self.n):
+			new_neighbours[i] = set()
+		ind_edges = []
+		while compt < self.n-1:
+			(j,k) = list_index_edges[sorted_index[ind]]
+			if j not in new_neighbours[k]:
+				ind_edges.append(sorted_index[ind])
+				s = new_neighbours[j].union(new_neighbours[k])
+				new_neighbours[j] = s.copy()
+				new_neighbours[j].add(k)
+				new_neighbours[k] = s.copy()
+				new_neighbours[k].add(j)
+				compt += 1
+			ind+=1
+		print(list_value_edges[ind])
+		
+		adj = np.zeros((self.n,self.n),dtype = 'bool')
+		edges_value = np.zeros((self.n,self.n))
+		for k in ind_edges :
+			(i,j) = list_index_edges[k]
+			adj[i][j] = True
+			adj[j][i] = True
+			v = list_value_edges[k]
+			edges_value[i][j] = v
+			edges_value[j][i] = v
+		G = Graph(self.n,adj,oriented = False, edges_value = edges_value)
+		return (G, sum(sum(edges_value))/2)
+
+
 
 
 
