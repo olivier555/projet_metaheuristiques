7afb85696fc1c321935ef09e41d9e60908149ca1
= <=bastien.rolland@eleves.enpc.fr>
2018-10-24 19:28:22 +0200

    genetic

--- local_search.py
+++ local_search.py
@@ -7,19 +7,19 @@ Created on Mon Oct 22 22:03:45 2018
 
 import numpy as np
 
-class LocalSearch:
+# class LocalSearch:
 
-    def remove_targets(self, solution, data):
-        M = data.get_matrix_sens()
-        nb_sens = np.matmul(M, solution.sensors.astype(int)) >= 2
-        sensors = solution.get_index_sensors()
-        for s in sensors:
-            if (np.extract(M[s], nb_sens)).all():
-                solution.remove_sensor(s)
-                if not solution.related(data):
-                    solution.add_sensor(s)
-                else:
-                    nb_sens = np.matmul(M, solution.sensors.astype(int)) >= 2
-        return solution
+def remove_targets(solution, data):
+    M = data.get_matrix_sens()
+    nb_sens = np.matmul(M, solution.sensors.astype(int)) >= 2
+    sensors = solution.get_index_sensors()
+    for s in sensors:
+        if (np.extract(M[s], nb_sens)).all():
+            solution.remove_sensor(s)
+            if not solution.related(data):
+                solution.add_sensor(s)
+            else:
+                nb_sens = np.matmul(M, solution.sensors.astype(int)) >= 2
+        # return solution
                 
             
